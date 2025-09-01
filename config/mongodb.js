9d102966823c8b4d6c79d1699961cb3c11fd5ffd
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-09-01 13:49:45 +0530

    Update mongodb.js

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () =>{
     mongoose.connection.on('connected',()=>console.log("Database connected"));
     await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
-useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true, //
+
 });
 
 };
