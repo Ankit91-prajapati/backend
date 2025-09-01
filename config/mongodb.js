a5190ceb95eb7a2d74b593caf81afd55339cfa41
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-09-01 14:03:57 +0530

    Update mongodb.js

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/mern-auth', {
+    await mongoose.connect(`{process.env.MONGODB_URI}/mern-auth`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
