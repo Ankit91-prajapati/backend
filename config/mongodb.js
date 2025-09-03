11c569c5a3df71eec7bc00223f65e9b2ac9a461b
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-09-03 08:02:29 +0530

    Update mongodb.js

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,16 +3,20 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`, {
-    useNewUrlParser: true,
-  useUnifiedTopology: true,
-   ssl: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
     });
 
     mongoose.connection.on('connected', () => {
-      console.log('MongoDB connected');
+      console.log('✅ MongoDB connected');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error('❌ MongoDB connection error:', err);
     });
   } catch (err) {
-    console.error('MongoDB connection error:', err);
+    console.error('❌ Initial MongoDB connection error:', err);
   }
 };
 
