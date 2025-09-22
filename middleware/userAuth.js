261343d97d08c34810644cffd2e7a23d4e0dee18
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-09-22 10:45:41 +0530

    New

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -4,13 +4,6 @@ const userAuth = (req, res, next) => {
   try {
     const token = req.cookies?.token;
 
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Not authorized. Please log in again.",
-      });
-    }
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.userId = decoded.id; // attach userId for controllers
